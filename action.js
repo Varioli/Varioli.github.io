2a1c244c1a7d7710c5b4b955d1722c23e058cacf
Varioli <90149482+Varioli@users.noreply.github.com>
2024-03-11 23:32:08 -0500

    Add files via upload

---
+++ action.js
@@ -0,0 +1,48 @@
+function navigateTo(url) {
+    window.location.href = url;
+}
+
+function changeColor(btn) {
+    btn.style.backgroundColor = '#363435';
+    btn.style.color = '#fdf9f9';
+    btn.style.border = '1px solid';
+}
+
+function resetColor(btn) {
+    btn.style.backgroundColor = '#8be3dd';
+    btn.style.color = '#363435';
+    btn.style.border = 'none';
+}
+
+window.onload = function() {
+    var nameElement = document.getElementById('myName');
+    nameElement.style.left = '50px';
+    animateButtons(); // Call the animateButtons function here
+}
+
+function animateButtons() {
+    const buttons = document.querySelectorAll('#menu button');
+    buttons.forEach((button, index) => {
+        setTimeout(() => {
+            button.classList.add('show');
+        }, index * 200); // Adjust delay between button animations
+    });
+}
+
+window.onload = function() {
+    var nameElement = document.getElementById('myName');
+    nameElement.style.left = '50px';
+    animateButtons(); // Call the animateButtons function here
+    
+    var bodyBoxElement = document.getElementById('bodyBox');
+    bodyBoxElement.classList.add('slide-and-fade-in');
+}
+
+function toggleContactPage() {
+        var contactPage = document.getElementById('contactPage');
+        if (contactPage.style.display === 'none') {
+            contactPage.style.display = 'block';
+        } else {
+            contactPage.style.display = 'none';
+        }
+    }
